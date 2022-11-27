3e1a16e473a68514d5aad0ccaab2fd48c6ff4ac3
bond007 <mihail.ivanov@inbox.ru>
2022-11-27 14:04:01 +0300

    Сделал домашнюю работу по семинару №1

---
+++ Homeworks/Homework 1/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 2 - поиск максимального из двух чисел
+/*Console.WriteLine("Enter number 1 ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number 2 ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2) Console.WriteLine($"Max = {number1}");
+else Console.WriteLine($"Max = {number2}"); */
+
+
+// Задача 4 - поиск максимального из 3-х чисел
+/*Console.WriteLine("Enter number 1 ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number 2 ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number 3 ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+
+if (number2 > max) max = number2;
+if(number3 > max) max = number3;
+
+Console.WriteLine($"max = {max}"); */
+
+
+// Задача 6 - определение четности
+/*Console.WriteLine("Enter number");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number%2 == 0) Console.WriteLine("number is chetnoe");
+else Console.WriteLine("number is not chetnoe"); */
+
+
+// Задача 8 - определение четности
+/*Console.WriteLine("Enter N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int number = 2;
+if (N == 1) Console.WriteLine("No numbers");                     // Если введенное число равно 1
+if (N <= 0)                                                       // Если введенное число отрицательное, либо равно 0
+    {
+        number = 0;
+        while (N <= number) 
+            {
+                 Console.Write(number + "  ");
+                 number = number - 2;
+            }
+    } 
+
+    else
+    {
+     while (N >= number) 
+        {
+        Console.Write(number + "  ");
+        number = number + 2;
+        }
+
+     } */
+
+
+
+
+
